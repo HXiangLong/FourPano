01505ac6a95fbeed59cfdab7c51b1cb67cf694f0
HXiangLong <hxl5202018@gmail.com>
2018-05-18 11:26:11 +0800

    webpack更新到4.6.1

--- webpack-config/devServer.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    contentBase: './public',
-    host: 'localhost',
-    port: 8081, // 默认8080
-    inline: true, // 可以监控js变化
-    hot: true, // 热启动
-    compress: true,
-    watchContentBase: false
-};
