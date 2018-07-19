84f1a1a66f4ca4340184ddea76c58835bfaf650e
HXiangLong <hxl5202018@gmail.com>
2018-07-19 17:20:13 +0800

    界面文件建立

--- app/views/PC/react/index.jsx
+++ app/views/PC/react/index.jsx
@@ -1,3 +1,5 @@
+/**初始化react */
+
 import React ,{Component}from 'react';
 import './index.pcss'
 
---
+++ app/views/PC/react/thumbnails/Thumbnails.jsx
@@ -0,0 +1,12 @@
+/**展厅列表 */
+
+import React ,{Component}from 'react';
+import './Thumbnails.pcss';
+
+class Thumbnails extends Component{
+    constructor(){
+        super();
+    }
+}
+
+export default Thumbnails;
